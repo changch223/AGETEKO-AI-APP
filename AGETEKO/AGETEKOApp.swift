5f5d971348f9821bd505e9c9747730f857551ccf
Chia-Wei <chang.ch23@gmail.com>
2025-03-18 13:42:43 -0400

    first commit

---
+++ AGETEKO/AGETEKOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGETEKOApp.swift
+//  AGETEKO
+//
+//  Created by chang chiawei on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct AGETEKOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AGETEKOTests/AGETEKOTests.swift
@@ -0,0 +1,16 @@
+//
+//  AGETEKOTests.swift
+//  AGETEKOTests
+//
+//  Created by chang chiawei on 2025-03-18.
+//
+
+import Testing
+
+struct AGETEKOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
